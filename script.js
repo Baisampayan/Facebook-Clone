296e1510bb15d92afb64d9ed645e9cb1a570f91b
Baisampayan <baisampayandas@gmail.com>
2023-06-13 02:45:42 +0530

    Dark Mode - Added

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ var SettingMenu = document.querySelector(".menu-setting");
 function SettingMenuToggle() {
     SettingMenu.classList.toggle("menu-setting-height");
 }
+
+var DarkButtonToggle = document.getElementById("dark-btn");
+DarkButtonToggle.onclick = function() {
+    DarkButtonToggle.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-mode");
+}
