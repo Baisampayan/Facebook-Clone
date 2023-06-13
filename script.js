c0f4502771fddc6865b45b223e0f18df20710291
Baisampayan <baisampayandas@gmail.com>
2023-06-14 01:39:49 +0530

    Initializing the localStorage

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ DarkButtonToggle.onclick = function() {
     DarkButtonToggle.classList.toggle("dark-btn-on");
     document.body.classList.toggle("dark-mode");
 }
+
+// Storing the value in the local Storage
+// localStorage.setItem("theme", "light");
+
+// Getting the value in the local Storage
+// localStorage.getItem("theme");
