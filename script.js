d7c15c3831d5386c2b7e0ea73fb4a8e597eea31d
Baisampayan <baisampayandas@gmail.com>
2023-06-14 01:54:38 +0530

    Dark/Light LocalStorage - Done

--- script.js
+++ script.js
@@ -8,6 +8,22 @@ var DarkButtonToggle = document.getElementById("dark-btn");
 DarkButtonToggle.onclick = function() {
     DarkButtonToggle.classList.toggle("dark-btn-on");
     document.body.classList.toggle("dark-mode");
+    // Storing the value in the local Storage
+    if(localStorage.getItem("theme") == "light") {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+}
+
+if(localStorage.getItem("theme") == "light") {
+    DarkButtonToggle.classList.remove("dark-btn-on");
+    document.body.classList.remove("dark-mode");
+} else if (localStorage.getItem("theme") == "dark") {
+    DarkButtonToggle.classList.add("dark-btn-on");
+    document.body.classList.add("dark-mode");
+} else {
+    localStorage.setItem("theme", "light");
 }
 
 // Storing the value in the local Storage
